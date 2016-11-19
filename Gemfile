3c7898dbe1a6a7cdaea56ccdcc65ccf70302da1f
Davide Laezza <truzzialrogo@gmx.com>
2016-11-19 16:23:35 +0100

    Added gem bundle system, some function and donw with color table

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+gem 'sass', '>=3.4.22'
+gem 'compass', '>=1.0.3'
+gem 'SassyLists', '>=2.2.5'
+gem 'sassy-maps', '>=0.4.0'
+gem 'sassy-strings', '>=1.0.0'
--- config.rb
+++ config.rb
@@ -1,5 +1,8 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+require 'SassyLists'
+require 'sassy-maps'
+require 'sassy-strings'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
