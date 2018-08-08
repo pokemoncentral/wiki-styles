b415ae051769d48a5880f3329901a1caed46ca07
Davide Laezza <truzzialrogo@gmx.com>
2018-08-08 22:28:46 +0200

    Adding styles-changing javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+-function($){
+    $(function() {
+        ['tiny', 'small', 'medium', 'large', 'huge'].forEach(size => {
+            const className = size + '-spacing';
+            var wrappers = $(`.${ className }`);
+            const items = wrappers.find('> *, td, th')
+            .add(`.${ className }-item`);
+            wrappers = wrappers.filter(':not(table)');
+
+            $(`[name="${ className }"]`).change(function() {
+                const val = $(this).val()
+                    .replace(/[\d\.]+/, n => parseFloat(n) / 2);
+
+                wrappers.css({margin: `-${ val }`});
+                items.css({padding: val});
+            })
+        });
+    });
+}(jQuery);
