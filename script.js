872016491422fce9ea15bd52089341821e1d0773
Davide Laezza <truzzialrogo@gmx.com>
2018-08-24 19:20:01 +0200

    Adding server interaction for voting

--- script.js
+++ script.js
@@ -1,20 +1,151 @@
 -function($){
 
+// axios.defaults.baseURL = 'http://maze0.mooo.com:20752';
+axios.defaults.baseURL = 'http://localhost:13980';
+axios.interceptors.response.use(null, error => alert(error));
+
+var name, form;
+const sizes = ['tiny', 'small', 'medium', 'large', 'huge'];
+
+const displayVote = (vote) => {
+    const row = $('<tr></tr>');
+    sizes.forEach(size => {
+        const value = vote[size] || 'none';
+        row.append(`<td>${ value }</td>`);
+    });
+    const table = $(`
+        <table id="single-vote">
+            <caption>My vote</caption>
+            <tr>
+                <th>Tiny</th>
+                <th>Small</th>
+                <th>Medium</th>
+                <th>Large</th>
+                <th>Huge</th>
+            </tr>
+        </table>`);
+    table.append(row);
+
+    $('#single-vote').remove();
+    form.after(table);
+};
+
+const displayVotes = votes => {
+    const rows = votes.map(row => $(`
+        <tr>
+            <td>${ row.size }</td>
+            <td>${ row.value }</td>
+            <td>${ row.voters.join(', ') }</td>
+        </tr>
+    `));
+    const table = $(`
+        <table id="all-votes">
+            <caption>All votes</caption>
+            <tr>
+                <th>Size</th>
+                <th>Value</th>
+                <th>Voters</th>
+            </tr>
+        </table>`);
+    table.append(rows);
+
+    $('#all-votes').remove();
+    form.after(table);
+};
+
+const getName = () => {
+    const voter = name.val().trim();
+
+    if (voter === '') {
+        alert('We need your name in order for you to vote');
+        throw Error('Missing name');
+    }
+
+    return voter;
+};
+
 $(function() {
-    ['tiny', 'small', 'medium', 'large', 'huge'].forEach(size => {
+    const inputs = sizes.reduce((acc, size) => {
+        const className = size + '-spacing';
+        acc[size] = $(`[name="${ className }"]`);
+        return acc;
+    }, {});
+    name = $('[name="voter-name"]');
+
+    sizes.forEach(size => {
         const className = size + '-spacing';
         var wrappers = $(`.${ className }`);
         const items = wrappers.find('> *, td, th')
             .add(`.${ className }-item`);
         wrappers = wrappers.filter(':not(table)');
 
-        $(`[name="${ className }"]`).change(function() {
-            const val = $(this).val()
-                .replace(/[\d\.]+/, n => parseFloat(n) / 2);
+        inputs[size].change(function() {
+            const spacing = $(this).val().trim();
+            if (spacing === '') {
+                return;
+            }
 
+            const val = spacing.replace(/[\d\.]+/, n => parseFloat(n) / 2);
             wrappers.css({margin: `-${ val }`});
             items.css({padding: val});
-        })
+        });
+    });
+
+    form = $('form[name="vote"]');
+    form.submit(function() {
+        try {
+            const voter = getName();
+        }
+        catch (e) {
+            return false;
+        }
+        const endpoint = `/votes/${ name.val().trim() }`;
+
+        const vote = Object.keys(inputs).reduce((acc, size) => {
+            const input = inputs[size];
+            const value = input.val().trim()
+            if (value !== '') {
+                acc[size] = value;
+            }
+            return acc;
+        }, {});
+
+        if ($('[name="overwrite-vote"]').is(':checked')) {
+            axios.put(endpoint, vote)
+                 .then(resp => displayVote(vote));
+        }
+        else {
+            axios.patch(endpoint, vote)
+                 .then(resp => displayVote(resp.data));
+        }
+
+        return false;
+    });
+
+    $('[name="get-vote"]').click(function() {
+        const voter = getName();
+        const endpoint = `/votes/${ voter }`;
+
+        axios.get(endpoint)
+             .then(resp => {
+                 const vote = resp.data;
+
+                 displayVote(vote);
+                 console.log(vote);
+                 sizes.forEach(size => {
+                     const value = vote[size];
+                     if (value) {
+                         const input = inputs[size];
+                         input.val(value);
+                         input.change();
+                     }
+                 });
+             });
+    });
+
+    $('[name="get-all"]').click(function() {
+        axios.get('/votes')
+             .then(resp => displayVotes(resp.data));
     });
 });
 
